027da707708044a1f115db5b8a77f2d1929dccb1
Mwihaki <karumba.mwihaki@gmail.com>
2022-09-18 04:06:27 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string in the puts function
+ *
+ * Description: using the main function
+ * This program prints"Programming is like building a multilingual puzzle
+ * Return: 0 if success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
