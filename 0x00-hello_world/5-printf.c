6be5fe0aea56a7e5085c560e9e99c1264121ec5f
Mwihaki <karumba.mwihaki@gmail.com>
2022-09-18 04:49:42 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string in the printf function
+ *
+ * Description: The string is printed to the stdout
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
